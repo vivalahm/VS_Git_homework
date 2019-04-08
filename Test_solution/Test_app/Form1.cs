14cd0e3130aeca8cc11152ea064a25771fec4d7b
HM <vivalahm@gmail.com>
2019-04-08 20:54:33 +0900

    change master

--- Test_solution/Test_app/Form1.cs
+++ Test_solution/Test_app/Form1.cs
@@ -16,5 +16,10 @@ namespace Test_app
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World!");
+        }
     }
 }
